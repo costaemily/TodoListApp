f0738d88c23512c9d9b0d94de5eddb2a21fdafba
costaemily <costa.emily@outlook.com>
2023-03-23 19:01:23 -0300

    add: controllers and models

---
+++ src/controllers/taskController.ts
@@ -0,0 +1,34 @@
+import firestore from '@react-native-firebase/firestore';
+import {Task} from '../models/task';
+
+const tasksCollection = firestore().collection('Tasks');
+
+export async function createTask(taskInfo: Task) {
+  await tasksCollection.add({
+    description: taskInfo.description,
+    dateIn: taskInfo.dateIn,
+    dateModification: taskInfo.dateModification,
+    isDone: taskInfo.isDone,
+  });
+}
+
+export async function consultTask(id: string) {
+  return await tasksCollection.doc(id).get();
+}
+
+export async function updateTask(id: string, taskInfo: Task) {
+  await tasksCollection.doc(id).update({
+    description: taskInfo.description,
+    dateIn: taskInfo.dateIn,
+    dateModification: taskInfo.dateModification,
+    isDone: taskInfo.isDone,
+  });
+}
+
+export async function deleteTask(id: string) {
+  await tasksCollection.doc(id).delete();
+}
+
+export async function listTasksOrderbyDateIn() {
+  return await tasksCollection.get();
+}
