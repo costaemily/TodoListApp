91ab33429570e205950b445745c58c609cbd0478
costaemily <costa.emily@outlook.com>
2023-03-23 17:36:24 -0300

    add: models

---
+++ src/models/task.ts
@@ -0,0 +1,19 @@
+class Task {
+  description: string;
+  dateIn: Date;
+  dateModification: Date;
+  isDone: boolean;
+
+  constructor(
+    description: string,
+    dateIn: Date,
+    dateModification: Date,
+    isDone: boolean,
+  ) {
+    this.description = description;
+    this.dateIn = dateIn;
+    this.dateModification = dateModification;
+    this.isDone = isDone;
+  }
+}
+export default Task;
